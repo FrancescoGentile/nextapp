244a2b61f13d4365fe83c78cb38c5904adaf4d39
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-20 11:25:16 +0200

    Updated services initialization

--- backend/src/modules/room/src/domain/services/index.ts
+++ backend/src/modules/room/src/domain/services/index.ts
@@ -2,20 +2,25 @@
 //
 //
 
+import { BookingRepository } from '../ports/booking.repository';
+import { BookingService } from '../ports/booking.service';
 import { EventBroker } from '../ports/event.broker';
 import { RoomRepository } from '../ports/room.repository';
 import { RoomInfoService } from '../ports/room.service';
 import { UserRepository } from '../ports/user.repository';
+import { NextBookingService } from './booking';
 import { NextRoomInfoService } from './room';
 import { NextUserService } from './user';
 
 export function init_services(
   user_repo: UserRepository,
   room_repo: RoomRepository,
+  booking_repo: BookingRepository,
   broker: EventBroker
-): { room_service: RoomInfoService } {
+): { room_service: RoomInfoService; booking_service: BookingService } {
   const room_service = new NextRoomInfoService(room_repo, user_repo);
+  const booking_service = new NextBookingService(booking_repo, room_repo);
   const _ = new NextUserService(broker, user_repo);
 
-  return { room_service };
+  return { room_service, booking_service };
 }
