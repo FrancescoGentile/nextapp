66c41f81dbec4b505933177cc9682d6858d2ab37
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-20 16:22:01 +0200

    Fixed bugs in room API

--- backend/src/modules/room/src/application/rest/routes/room.ts
+++ backend/src/modules/room/src/application/rest/routes/room.ts
@@ -9,7 +9,19 @@ import { toJson } from 'json-joi-converter';
 import express from 'express';
 import { RoomNotFound } from '../../../domain/errors';
 import { Room, RoomID } from '../../../domain/models/room';
-import { asyncHandler } from '../utils';
+import { API_VERSION, asyncHandler } from '../utils';
+
+const BASE_PATH = '/rooms';
+
+function room_to_json(room: Room): any {
+  return {
+    self: `${API_VERSION}${BASE_PATH}/${room.id!.to_string()}`,
+    name: room.name,
+    details: room.details,
+    seats: room.seats,
+    floor: room.floor,
+  };
+}
 
 async function get_room(request: Request, response: Response) {
   const id = RoomID.from_string(request.params.room_id);
@@ -17,7 +29,7 @@ async function get_room(request: Request, response: Response) {
   if (rooms.length === 0) {
     throw new RoomNotFound(id.to_string());
   }
-  response.status(StatusCodes.OK).json(rooms[0]);
+  response.status(StatusCodes.OK).json(room_to_json(rooms[0]));
 }
 
 async function get_rooms(request: Request, response: Response) {
@@ -29,7 +41,7 @@ async function get_rooms(request: Request, response: Response) {
 
   const ids: RoomID[] = value.map((id: string) => RoomID.from_string(id));
   const rooms = await request.room_service!.get_rooms(ids);
-  response.status(StatusCodes.OK).json(rooms);
+  response.status(StatusCodes.OK).json(rooms.map(room_to_json));
 }
 
 async function create_room(request: Request, response: Response) {
@@ -52,7 +64,10 @@ async function create_room(request: Request, response: Response) {
     new Room(value.name, value.details, value.seats, value.floor)
   );
 
-  response.status(StatusCodes.CREATED).location(request.path + id.to_string());
+  response
+    .status(StatusCodes.CREATED)
+    .location(`${API_VERSION}${BASE_PATH}/${id.to_string()}`)
+    .send();
 }
 
 async function delete_room(request: Request, response: Response) {
@@ -66,10 +81,10 @@ async function delete_room(request: Request, response: Response) {
 export function init_room_routes(): express.Router {
   const router = express.Router();
 
-  router.get('/rooms/:room_id', asyncHandler(get_room));
-  router.get('/rooms', asyncHandler(get_rooms));
-  router.post('/rooms', asyncHandler(create_room));
-  router.delete('/rooms/:room_id', asyncHandler(delete_room));
+  router.get(`${BASE_PATH}/:room_id`, asyncHandler(get_room));
+  router.get(BASE_PATH, asyncHandler(get_rooms));
+  router.post(BASE_PATH, asyncHandler(create_room));
+  router.delete(`${BASE_PATH}/:room_id`, asyncHandler(delete_room));
 
   return router;
 }
