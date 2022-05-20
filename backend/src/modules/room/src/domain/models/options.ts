779e96a62630608fdec1401ddb9bac32f116c046
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-20 11:54:47 +0200

    Added constraints to option

--- backend/src/modules/room/src/domain/models/options.ts
+++ backend/src/modules/room/src/domain/models/options.ts
@@ -2,17 +2,35 @@
 //
 //
 
+import { InvalidRequestError } from '@nextapp/common/error';
+
 /**
  * When a user wants to retrieve some entities,
- * they need to specify the offset and the number of entities.
+ * they can specify the offset and the number of entities.
  */
 export class SearchOptions {
-  public readonly offset: number;
+  public static readonly DEFAULT_OFFSET = 0;
 
-  public readonly limit: number;
+  public static readonly DEFAULT_LIMIT = 20;
 
-  public constructor(offset: number = 0, limit: number = 20) {
-    this.offset = offset;
-    this.limit = limit;
+  private constructor(
+    // offset is an integer greater than or equal to 0
+    public readonly offset: number,
+    // limit is an integer greater than or equal to 1
+    public readonly limit: number
+  ) {}
+
+  public static build(offset: number, limit: number): SearchOptions {
+    if (!Number.isInteger(offset) || offset < 0) {
+      throw new InvalidRequestError(
+        `Offset should be an integer and greater than or equal to 0`
+      );
+    }
+    if (!Number.isInteger(limit) || limit < 1) {
+      throw new InvalidRequestError(
+        `Limit should be an integer and greater than or equal to 1`
+      );
+    }
+    return new SearchOptions(offset, limit);
   }
 }
