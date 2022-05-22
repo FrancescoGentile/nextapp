18e7113d748ca32871e5102522c7610286fb0c06
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2022-05-22 12:56:00 +0200

    modified:   backend/src/modules/user/src/domain/events/events.index.ts

--- backend/src/modules/user/src/domain/events/events.index.ts
+++ backend/src/modules/user/src/domain/events/events.index.ts
@@ -2,9 +2,8 @@
 //
 //
 
-import { NextEvent, ModuleID } from '@nextapp/common/event';
+import { NextEvent } from '@nextapp/common/event';
 import { UserID, UserRole } from '@nextapp/common/user';
-import { DateTime } from 'luxon';
 
 export interface UserCreatedEvent extends NextEvent {
   user_id: UserID;
@@ -14,4 +13,8 @@ export interface UserCreatedEvent extends NextEvent {
 export interface UserRoleChangedEvent extends NextEvent {
   user_id: UserID;
   role: UserRole;
+}
+
+export interface UserLogin extends NextEvent {
+  user_id: UserID;
 }
