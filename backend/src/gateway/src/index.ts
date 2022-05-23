56878fa2b3c8f1694e249c6ccfe31bae006190fd
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-23 09:42:55 +0200

    Added server initialization

---
+++ backend/src/gateway/src/index.ts
@@ -0,0 +1,73 @@
+//
+//
+//
+
+import express from 'express';
+import { Request, Response } from 'express-serve-static-core';
+import neo4j, { Driver } from 'neo4j-driver';
+import { InternalServerError } from '@nextapp/common/error';
+import { EventEmitter } from 'eventemitter3';
+import cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import { InvalidEndpoint } from './errors';
+
+async function get_neo4j(): Promise<Driver> {
+  const url = process.env.NEO4J_URL;
+  const username = process.env.NEO4J_USERNAME;
+  const pwd = process.env.NEO4J_PWD;
+  if (url === undefined || username === undefined || pwd === undefined) {
+    throw new InternalServerError('Missing Neo4J parameters.');
+  }
+
+  const driver = neo4j.driver(url, neo4j.auth.basic(username, pwd));
+  try {
+    await driver.verifyConnectivity();
+  } catch {
+    throw new InternalServerError('It was not possible to connect to Neo4J.');
+  }
+
+  return driver;
+}
+
+async function init_gateway(): Promise<express.Router> {
+  const driver = await get_neo4j();
+  const emitter = new EventEmitter();
+
+  const router = express.Router();
+  router.use(cookieParser() as any);
+
+  return router;
+}
+
+function invalid_endpoint(request: Request, response: Response) {
+  const error = new InvalidEndpoint(request.url);
+  error.instance = request.url;
+  response.status(error.code).json(error);
+}
+
+async function start_server(port: number) {
+  const router = await init_gateway();
+  const app = express();
+
+  app.use(router);
+  app.use(invalid_endpoint);
+
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server listening on port ${port}`);
+  });
+}
+
+async function main() {
+  const port = Number.parseInt(
+    process.env.PORT ? process.env.PORT : '8080',
+    10
+  );
+
+  await start_server(port);
+}
+
+main()
+  .then()
+  // eslint-disable-next-line no-console
+  .catch((e) => console.log(e));
