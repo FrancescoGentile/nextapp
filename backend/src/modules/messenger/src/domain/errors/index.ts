355ce40b50146b0ac7ab50818cdd7112e6754ad9
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-29 12:14:05 +0200

    Added EmailID

--- backend/src/modules/messenger/src/domain/errors/index.ts
+++ backend/src/modules/messenger/src/domain/errors/index.ts
@@ -6,19 +6,32 @@ import { NextError, StatusCodes } from '@nextapp/common/error';
 
 enum EmailErrorTypes {
   INVALID_EMAIL = 1,
+  INVALID_ID,
 }
 
-function get_user_type(type: EmailErrorTypes): string {
+function get_email_type(type: EmailErrorTypes): string {
   return `user-${String(type).padStart(3, '0')}`;
 }
 
 export class InvalidEmail extends NextError {
   public constructor(email: string, options?: ErrorOptions) {
     super(
-      get_user_type(EmailErrorTypes.INVALID_EMAIL),
+      get_email_type(EmailErrorTypes.INVALID_EMAIL),
       StatusCodes.BAD_REQUEST,
       'Invalid email',
       `${email} is not a valid email.`,
+      options
+    );
+  }
+}
+
+export class InvalidEmailID extends NextError {
+  public constructor(id: string, options?: ErrorOptions) {
+    super(
+      get_email_type(EmailErrorTypes.INVALID_ID),
+      StatusCodes.BAD_REQUEST,
+      'Invalid id',
+      `${id} is not a valid id for an email.`,
       options
     );
   }
