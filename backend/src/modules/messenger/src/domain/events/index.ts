7de7e1641a65a67b135ca0f553d3a9558af17c55
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-29 10:03:36 +0200

    Implemented user service

--- backend/src/modules/messenger/src/domain/events/index.ts
+++ backend/src/modules/messenger/src/domain/events/index.ts
@@ -8,7 +8,7 @@ import { UserID, UserRole } from '@nextapp/common/user';
 export interface UserCreatedEvent extends NextEvent {
   id: UserID;
   role: UserRole;
-  name: string;
+  user_name: string;
   email: string;
   password: string;
 }
