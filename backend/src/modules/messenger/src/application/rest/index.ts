2be15b57d337d6b6b296740cdf5b0f424b1b2347
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2022-05-30 09:24:19 +0200

    Implemented rest api structure

---
+++ backend/src/modules/messenger/src/application/rest/index.ts
@@ -0,0 +1,47 @@
+//
+//
+//
+
+import { InternalServerError, NextError } from '@nextapp/common/error';
+import express from 'express';
+import { NextFunction, Request, Response } from 'express-serve-static-core';
+import { UserInfoService } from '../../domain/ports/info.service';
+
+function init_request(
+  info_service: UserInfoService
+): (req: Request, res: Response, next?: NextFunction) => void {
+  return (req: Request, _res: Response, next?: NextFunction) => {
+    req.info_service = info_service;
+    next!();
+  };
+}
+
+function handle_error(
+  e: Error,
+  req: Request,
+  res: Response,
+  _next?: NextFunction
+) {
+  let error;
+  if (e instanceof NextError) {
+    error = e;
+  } else {
+    error = new InternalServerError();
+  }
+
+  error.instance = req.url;
+  res.status(error.code).json(error);
+}
+
+export function init_rest_api(info_service: UserInfoService): express.Router {
+  const router = express.Router();
+
+  router.use(express.urlencoded() as any);
+  router.use(express.json() as any);
+
+  router.use(init_request(info_service));
+
+  router.use(handle_error);
+
+  return router;
+}
---
+++ backend/src/modules/messenger/src/application/rest/request.ts
@@ -0,0 +1,14 @@
+//
+//
+//
+
+import { UserID } from '@nextapp/common/user';
+import {} from 'express-serve-static-core';
+import { UserInfoService } from '../../domain/ports/info.service';
+
+declare module 'express-serve-static-core' {
+  export interface Request {
+    user_id: UserID;
+    info_service: UserInfoService;
+  }
+}
