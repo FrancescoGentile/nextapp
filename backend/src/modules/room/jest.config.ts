d7a1310b5c013f002d33b2c3a0bda28900eecbeb
ManuelaCorte <manu.cortepause@gmail.com>
2022-06-02 22:00:28 +0200

    Setup of testing files

---
+++ backend/src/modules/room/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+// Or async function
+export default async (): Promise<Config.InitialOptions> => ({
+  verbose: true,
+  preset: 'ts-jest',
+  roots: ['./tests'],
+  setupFiles: ['./.jest/setenv.ts'],
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'src',
+  coverageReporters: ['json-summary'],
+});
