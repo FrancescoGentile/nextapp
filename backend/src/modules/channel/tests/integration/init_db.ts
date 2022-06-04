ca6b50314855eaa4b32fad81d7af35aadd0f1ade
Riccardo Gennaro <75367744+lallo-unitn@users.noreply.github.com>
2022-06-04 20:24:03 +0200

    Finished init_db

--- backend/src/modules/channel/tests/integration/init_db.ts
+++ backend/src/modules/channel/tests/integration/init_db.ts
@@ -108,22 +108,53 @@ async function populate_subscriptions(
 ): Promise<Sub[]> {
   const subscriptions: Sub[] = [
     {
-      id: SubID.from_string('1111111111'),
-      user: users[3].id,
+      id: SubID.from_string('sub2to0'),
+      user: users[2].id,
       channel: channels[0].id!,
     },
     {
-      id: SubID.from_string('2222222222'),
+      id: SubID.from_string('sub4to0'),
       user: users[4].id,
       channel: channels[0].id!,
     },
     {
-      id: SubID.from_string('3333333333'),
+      id: SubID.from_string('sub3to1'),
       user: users[3].id,
       channel: channels[1].id!,
     },
   ];
-
+  // Subscribe every president
+  for (const channel of channels) {
+    for(const pres of channel.presID_array){
+      const session = driver.session();
+      const sub_id = `sub${pres.to_string()}to${channel.id!.to_string()}`
+      const subscription: Sub = 
+      {
+        id: SubID.from_string(sub_id),
+        user: pres, 
+        channel: channel.id!
+      };
+      try {
+        await session.writeTransaction((tx) =>
+          tx.run(
+            `MATCH (u:CHANNEL_User), (c:CHANNEL_Channel)
+              WHERE u.id = $user_id AND c.id = $channel_id
+              CREATE (u)-[s:CHANNEL_SUB { id: $subscription_id }]-(c)`,
+            {
+              user_id: subscription.user.to_string(),
+              channel_id: subscription.channel.to_string(),
+              subscription_id: subscription.id!.to_string(),
+            }
+          )
+        );
+      } catch {
+        throw new InternalServerError();
+      } finally {
+        await session.close();
+      }
+    }
+  }
+  //subscribe other users
   for (const subscription of subscriptions) {
     const session = driver.session();
     try {
@@ -166,7 +197,7 @@ async function clear_users(driver: Driver) {
   try {
     await session.writeTransaction((tx) =>
       tx.run(
-        `MATCH (u:USER_User) 
+        `MATCH (u:CHANNEL_User) 
          DETACH DELETE u`
       )
     );
@@ -177,8 +208,24 @@ async function clear_users(driver: Driver) {
   }
 }
 
+async function clear_channels(driver: Driver) {
+  const session = driver.session();
+  try {
+    await session.writeTransaction((tx) =>
+      tx.run(
+        `MATCH (c: CHANNEL_Channel) 
+         DETACH DELETE c`
+      )
+    );
+  } catch {
+    throw new InternalServerError();
+  } finally {
+    await session.close();
+  }
+}
+
 export async function clear_db(driver: Driver) {
-  await clear_users(driver);
+  await Promise.all([clear_users(driver), clear_channels(driver)]);
 }
 
 export async function init_driver(): Promise<Driver> {
