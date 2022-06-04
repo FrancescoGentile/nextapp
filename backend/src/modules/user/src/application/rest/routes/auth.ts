6c36ebc035fce3813fc8908ff7daefdc7a5e82f2
ManuelaCorte <manu.cortepause@gmail.com>
2022-06-04 16:56:31 +0200

    Added endpoint to reset password

--- backend/src/modules/user/src/application/rest/routes/auth.ts
+++ backend/src/modules/user/src/application/rest/routes/auth.ts
@@ -9,6 +9,7 @@ import express from 'express';
 import { DateTime } from 'luxon';
 import { asyncHandler, AuthMiddleware, COOKIE_NAME, validate } from '../utils';
 import { AuthToken } from '../../../domain/models/auth';
+import { Username } from '../../../domain/models/credentials';
 
 async function login(request: Request, response: Response) {
   const schema = Joi.object({
@@ -41,6 +42,19 @@ async function login(request: Request, response: Response) {
   }
 }
 
+async function forgot_password(request: Request, response: Response) {
+  const schema = Joi.object({
+    username: Joi.string().required(),
+  });
+
+  const value = validate(schema, request.body);
+  await request.user_service.forgot_password(
+    Username.from_string(value.username)
+  );
+
+  response.sendStatus(StatusCodes.ACCEPTED);
+}
+
 function logout(request: Request, response: Response) {
   response.status(StatusCodes.NO_CONTENT).clearCookie(COOKIE_NAME).send();
 }
@@ -51,7 +65,7 @@ export function init_auth_routes(
   const router = express.Router();
 
   router.post('/login', asyncHandler(login));
-
+  router.post('/forgot-password', asyncHandler(forgot_password));
   router.post('/logout', auth_middleware, asyncHandler(logout));
 
   return router;
