e2a471be91671d28454e7a8f297208fb55324b40
ManuelaCorte <manu.cortepause@gmail.com>
2022-06-05 11:13:02 +0200

    Fixed rest API

--- backend/src/modules/channel/src/application/rest/index.ts
+++ backend/src/modules/channel/src/application/rest/index.ts
@@ -9,15 +9,18 @@ import { init_channel_routes } from './routes/channel';
 import { init_sub_routes } from './routes/sub';
 import { ChannelInfoService } from '../../domain/ports/channel.service';
 import { SubService } from '../../domain/ports/sub.service';
-import { API_VERSION } from './utils';
+import { init_news_routes } from './routes/news';
+import { NewsInfoService } from '../../domain/ports/news.service';
 
 function init_request(
   channel_service: ChannelInfoService,
-  sub_service: SubService
+  sub_service: SubService,
+  news_service: NewsInfoService
 ): (req: Request, res: Response, next?: NextFunction) => void {
   return (req: Request, _res: Response, next?: NextFunction) => {
     req.channel_service = channel_service;
     req.sub_service = sub_service;
+    req.news_service = news_service;
     next!();
   };
 }
@@ -41,16 +44,18 @@ function handle_error(
 
 export function init_rest_api(
   channel_service: ChannelInfoService,
-  sub_service: SubService
+  sub_service: SubService,
+  news_service: NewsInfoService
 ): { router: express.Router } {
   const router = express.Router();
 
   router.use(express.urlencoded() as any);
   router.use(express.json() as any);
-  router.use(init_request(channel_service, sub_service));
-  
-  router.use(API_VERSION, init_channel_routes(), handle_error);
-  router.use(API_VERSION, init_sub_routes(), handle_error);
+  router.use(init_request(channel_service, sub_service, news_service));
+
+  router.use(init_channel_routes(), handle_error);
+  router.use(init_sub_routes(), handle_error);
+  router.use(init_news_routes(), handle_error);
 
   return { router };
 }
--- backend/src/modules/channel/src/application/rest/routes/news.ts
+++ backend/src/modules/channel/src/application/rest/routes/news.ts
@@ -1,123 +1,173 @@
+//
+//
+//
 
-import { News } from '../../../domain/models/news';
-import { API_VERSION, asyncHandler, validate } from '../utils';
 import { StatusCodes } from '@nextapp/common/error';
 import { Request, Response } from 'express-serve-static-core';
 import Joi from 'joi';
 import express from 'express';
-import { NewsID } from '../../../domain/models/news';
-import { NewsNotFound } from '../../../domain/errors';
+import { DateTime } from 'luxon';
+import { asyncHandler, validate } from '../utils';
+import { News, NewsID } from '../../../domain/models/news';
+import { ChannelNotFound, NewsNotFound } from '../../../domain/errors';
+import { SearchOptions } from '../../../domain/models/search';
+import { ChannelID } from '../../../domain/models/channel';
 
-const BASE_PATH = '/news';
+function id_to_self(id: NewsID): string {
+  return `news/${id.to_string()}`;
+}
 
-function news_to_json(news:News): any {
-    return {
-        self: `${API_VERSION}${BASE_PATH}/${news.id!.to_string()}`,
-        id: news.id!.to_string(),
-        title: news.title,
-        author: news.author,
-        date: news.date.toString(),
-        body: news.body,
-        channel: news.channel.toString()
-    };
+function news_to_json(news: News): any {
+  return {
+    self: id_to_self(news.id!),
+    channel: {
+      self: news.channel.to_string(),
+    },
+    author: {
+      self: news.author.to_string,
+    },
+    date: news.date.toISO(),
+    title: news.title,
+    body: news.body,
+  };
 }
 
 async function get_news_list(request: Request, response: Response) {
-    const news = await request.news_service!.get_news_list(request.user_id!);
-    response.status(StatusCodes.OK).json(news);
+  const schema = Joi.object({
+    offset: Joi.number(),
+    limit: Joi.number(),
+  });
+  const value = validate(schema, {
+    offset: request.query.offset,
+    limit: request.query.limit,
+  });
+  const options = SearchOptions.build(value.offset, value.limit);
+
+  const news = await request.news_service!.get_news_list(
+    request.user_id!,
+    options
+  );
+  response.status(StatusCodes.OK).json(news.map(news_to_json));
 }
 
 async function get_news_list_by_channel(request: Request, response: Response) {
-    const news = await request.news_service!.get_news_list_by_channel(request.user_id!, request.params.channel_id);
-    response.status(StatusCodes.OK).json(news);
+  let id: ChannelID;
+  try {
+    id = ChannelID.from_string(request.params.channel_id);
+  } catch {
+    throw new ChannelNotFound(request.params.channel_id);
+  }
+
+  const schema = Joi.object({
+    offset: Joi.number(),
+    limit: Joi.number(),
+  });
+  const value = validate(schema, {
+    offset: request.query.offset,
+    limit: request.query.limit,
+  });
+  const options = SearchOptions.build(value.offset, value.limit);
+
+  const news = await request.news_service!.get_news_list_by_channel(
+    request.user_id!,
+    id,
+    options
+  );
+  response.status(StatusCodes.OK).json(news);
 }
 
 async function get_single_news(request: Request, response: Response) {
-    let id;
-    try {
-        id = NewsID.from_string(request.params.news_id);
-    } catch {
-        throw new NewsNotFound(request.params.news_id);
-    }
+  let id;
+  try {
+    id = NewsID.from_string(request.params.news_id);
+  } catch {
+    throw new NewsNotFound(request.params.news_id);
+  }
 
-    const event = await request.news_service!.get_news_info(request.user_id!,id);
+  const news = await request.news_service!.get_news_info(request.user_id!, id);
+  response.status(StatusCodes.OK).json(news_to_json(news));
 }
 
-
-
 async function create_news(request: Request, response: Response) {
-    const schema = Joi.object({
-        title: Joi.string().required(),
-        author: Joi.string().required(),
-        date: Joi.date().required(),
-        body: Joi.string().required(),
-        channel: Joi.string().required()
-       
-    });
+  const schema = Joi.object({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+  });
 
-    
-    const value = validate(schema, request.body);
+  const value = validate(schema, request.body);
 
-    const id = await request.news_service!.create_news(
-        request.user_id!,value.channel,
-        new News(value.title,value.author, value.date, value.body, value.channel)
-    );
+  let channel_id: ChannelID;
+  try {
+    channel_id = ChannelID.from_string(request.query.channel_id);
+  } catch {
+    throw new ChannelNotFound(request.query.channel_id);
+  }
 
-    response
-        .status(StatusCodes.CREATED)
-        .location(`${API_VERSION}${BASE_PATH}/${id.to_string()}`)
-        .end();
+  const news = new News(
+    channel_id,
+    request.user_id!,
+    DateTime.utc(),
+    value.title,
+    value.body
+  );
+
+  const id = await request.news_service!.create_news(news);
+
+  response.status(StatusCodes.CREATED).location(id_to_self(id)).end();
 }
 
 async function update_news(request: Request, response: Response) {
-    const schema = Joi.object({
-        name: Joi.string().required(),
-        channel: Joi.string().required(),
-        description: Joi.string().required(),
-        start: Joi.string().required(),
-        end: Joi.string().required(),
-        room: Joi.string().required()
-    });
+  const schema = Joi.object({
+    title: Joi.string(),
+    body: Joi.string(),
+  });
 
-    const value = validate(schema, request.body);
+  const value = validate(schema, request.body);
 
-    let id;
-    try {
-      id = NewsID.from_string(request.params.event_id);
-    } catch {
-      throw new NewsNotFound(request.params.event_id);
-    }
-  
-    await request.news_service!.update_news(
-        request.user_id!,
-        id,
-        value.title,
-        value.date,
-        value.body
-    );
-  
-    response.sendStatus(StatusCodes.NO_CONTENT);
+  let id;
+  try {
+    id = NewsID.from_string(request.params.event_id);
+  } catch {
+    throw new NewsNotFound(request.params.event_id);
+  }
+
+  await request.news_service!.update_news(
+    request.user_id!,
+    id,
+    value.title,
+    value.body
+  );
+
+  response.sendStatus(StatusCodes.NO_CONTENT);
 }
 
 async function delete_news(request: Request, response: Response) {
-    await request.news_service!.delete_news(
-      request.user_id!,
-      NewsID.from_string(request.params.event_id)
-    );
-    response.sendStatus(StatusCodes.NO_CONTENT);
+  let id: NewsID;
+  try {
+    id = NewsID.from_string(request.params.event_id);
+  } catch {
+    throw new NewsNotFound(request.params.event_id);
+  }
+
+  await request.news_service!.delete_news(request.user_id!, id);
+  response.sendStatus(StatusCodes.NO_CONTENT);
 }
 
+export function init_news_routes(): express.Router {
+  const router = express.Router();
 
+  router.get('/news/:news_id', asyncHandler(get_single_news));
+  router.get('/news', asyncHandler(get_news_list));
+  router.get(
+    '/channels/:channel_id/news',
+    asyncHandler(get_news_list_by_channel)
+  );
 
-export function init_event_routes(): express.Router{
-    const router = express.Router();
+  router.post('/channel/:channel_id/news', asyncHandler(create_news));
 
-    router.get(`${BASE_PATH}`, asyncHandler(get_news_list));
-    router.get(`${BASE_PATH}/:room_id`, asyncHandler(get_single_news));
-    router.get(`/:channel_id/${BASE_PATH}`, asyncHandler(get_news_list_by_channel));
-    router.post(`${BASE_PATH}`, asyncHandler(create_news));
-    router.put(`${BASE_PATH}/:news_id`, asyncHandler(update_news));
-    router.delete(`${BASE_PATH}/:news_id`, asyncHandler(delete_news));
+  router.patch('/news/:news_id', asyncHandler(update_news));
 
-    return router;
-}   
+  router.delete('/news/:news_id', asyncHandler(delete_news));
+
+  return router;
+}
