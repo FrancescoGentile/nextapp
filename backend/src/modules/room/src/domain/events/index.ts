148223879b78577546241c7e7a1a1f679d96b756
ManuelaCorte <manu.cortepause@gmail.com>
2022-06-06 12:57:24 +0200

    Modified event broker

--- backend/src/modules/room/src/domain/events/index.ts
+++ backend/src/modules/room/src/domain/events/index.ts
@@ -4,6 +4,7 @@
 
 import { NextEvent } from '@nextapp/common/event';
 import { UserID, UserRole } from '@nextapp/common/user';
+import { DateTime } from 'neo4j-driver-core';
 
 export interface UserCreatedEvent extends NextEvent {
   user_id: UserID;
@@ -20,4 +21,29 @@ export interface UserDeletedEvent extends NextEvent {
 export interface UserRoleChangedEvent extends NextEvent {
   user_id: UserID;
   role: UserRole;
+}
+
+export interface CreateBookingRequestEvent extends NextEvent {
+  request_id: string;
+  requester_id: string;
+  room_id: string;
+  start: DateTime;
+  end: DateTime;
+}
+
+export interface CreateBookingResponseEvent extends NextEvent {
+  request_id: string;
+  confirmed: boolean;
+  booking_id?: string; // this is set only if confirmed is set to true
+}
+
+export interface DeleteBookingRequestEvent extends NextEvent {
+  request_id: string;
+  requester_id: string;
+  booking_id: string;
+}
+
+export interface DeleteBookingResponseEvent extends NextEvent {
+  request_id: string;
+  confirmed: boolean;
 }
